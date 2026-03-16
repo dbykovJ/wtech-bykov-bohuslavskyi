2307ad272477735343f0f7b7881186a3f1812a39
Yehor Bohuslavskyi <yehorbohuslavskyi@gmail.com>
2026-03-16 13:28:55 +0100

    Add category view template; implement basic layout and structure

---
+++ resources/views/category.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Search')
+@section('content')
+    <section class="max-w-[1160px] mx-auto px-4 md:px-8 pt-10 md:pt-12 pb-10">
+
+    </section>
+@endsection
